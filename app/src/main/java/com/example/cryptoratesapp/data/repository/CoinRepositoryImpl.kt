e9830405193800d0c4c47d55339b05c292d29203
Nikolai Lisenko <kollysenko@gmail.com>
2022-04-08 23:52:45 +0500

    Renaming classes Moving classes to the right packages Added Mapper Class Added logic for repository Updated dependencies

---
+++ app/src/main/java/com/example/cryptoratesapp/data/repository/CoinRepositoryImpl.kt
@@ -0,0 +1,48 @@
+package com.example.cryptoratesapp.data.repository
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import com.example.cryptoratesapp.data.database.AppDatabase
+import com.example.cryptoratesapp.data.mapper.CoinMapper
+import com.example.cryptoratesapp.data.network.ApiFactory
+import com.example.cryptoratesapp.domain.CoinInfo
+import com.example.cryptoratesapp.domain.CoinRepository
+import kotlinx.coroutines.delay
+
+class CoinRepositoryImpl(
+    private val application: Application
+) : CoinRepository {
+
+    private val coinInfoDao = AppDatabase.getInstance(application).coinPriceInfoDao()
+
+    private val apiService = ApiFactory.apiService
+
+    private val mapper = CoinMapper()
+
+    override fun getCoinInfoList(): LiveData<List<CoinInfo>> {
+        return Transformations.map(coinInfoDao.getPriceList()) {
+            it.map {
+                mapper.mapDbModelToEntity(it)
+            }
+        }
+    }
+
+    override fun getCoinInfo(fromSymbol: String): LiveData<CoinInfo> {
+        return Transformations.map(coinInfoDao.getPriceInfoAboutCoin(fromSymbol)) {
+            mapper.mapDbModelToEntity(it)
+        }
+    }
+
+    override suspend fun loadData() {
+        while (true) {
+            val topCoins = apiService.getTopCoinsInfo(limit = 50)
+            val fSyms = mapper.mapNamesListToString(topCoins)
+            val jsonContainer = apiService.getFullPriceList(fSyms = fSyms)
+            val coinInfoDtoList = mapper.mapJsonContainerToListCoinInfo(jsonContainer)
+            val dbModelList = coinInfoDtoList.map { mapper.mapDtoToDbModel(it) }
+            coinInfoDao.insertPriceList(dbModelList)
+            delay(10000)
+        }
+    }
+}
